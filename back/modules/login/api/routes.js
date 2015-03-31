42ff011538e472bfb9b8d679a3a01b4da2c99dc9
Nicolas Ibanheiz <nicolas.ibanheiz@gmail.com>
2015-03-31 18:23:51 -0300

    Sinc

--- back/modules/login/api/routes.js
+++ back/modules/login/api/routes.js
@@ -1,35 +1,29 @@
 module.exports = function (express, passport) {
   var router = express.Router();
 
+  function cb(user, info, req, res) {
+    var data = {
+      user: user,
+      message: info ? info.message : ''
+    };
+    req.logIn(user, function (err) {
+      if (err) {
+        data.err = err;
+      }
+      console.log(data);
+      return res.json(data);
+    });
+  }
+
   router.post('/login', function (req, res, next) {
     passport.authenticate('login', function (err, user, info) {
-      var data = {
-        user: user,
-        message: info ? info.message : ''
-      };
-      req.logIn(user, function (err) {
-        if (err) {
-          data.err = err;
-        }
-        console.log(data);
-        return res.json(data);
-      });
+      cb(user, info, req, res);
     })(req, res, next);
   });
 
   router.post('/signup', function (req, res, next) {
     passport.authenticate('signup', function (err, user, info) {
-      var data = {
-        user: user,
-        message: info ? info.message : ''
-      };
-      req.logIn(user, function (err) {
-        if (err) {
-          data.err = err;
-        }
-        console.log(data);
-        return res.json(data);
-      });
+      cb(user, info, req, res);
     })(req, res, next);
   });
 
