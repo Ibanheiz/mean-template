f1eab084cb9ba4607d96aa9c7c6296334a3446f0
ibanheiz <nicolas.ibanheiz@gmail.com>
2015-05-29 16:13:41 -0300

    Adicionado gulp-modular-tasks para reutilizar as tasks do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,28 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var stylus = require('gulp-stylus');
-var path = require('path');
-var gutil = require('gulp-util');
-var minifyCss = require('gulp-minify-css');
-var imagemin = require('gulp-imagemin');
-var nodemon = require('gulp-nodemon');
-var rename = require('gulp-rename');
-var livereload = require('gulp-livereload');
-var styleFiles = 'front/css/stylus/*.styl';
-var styleFilesModules = 'front/css/stylus/**/*.styl';
-var jsFiles = [
-  'front/js/*.js',
-  'front/js/modules/**/*.js'
-];
-var jsComponents = [
-  'front/components/**/angular.min.js',
-  'front/components/**/*min.js',
-];
-var jsComponentsMaps = [
-  'front/components/**/*min.js.map',
-];
+var modularGulp = require('gulp-modular-tasks')(gulp);
 
-gulp.task('lint', function () {
-  gulp.src(jsFiles)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+var styleFiles = 'front/css/stylus/*.styl',
+  styleFilesModules = 'front/css/stylus/**/*.styl',
+  jsFiles = [
+    'front/js/*.js',
+    'front/js/modules/**/*.js'
+  ],
+  jsComponents = [
+    'front/components/**/angular.min.js',
+    'front/components/**/*min.js',
+  ],
+  jsComponentsMaps = [
+    'front/components/**/*min.js.map',
+  ];
 
-gulp.task('stylus', function () {
-  gulp.src(styleFiles)
-    .pipe(stylus({
-      paths: [ path.join(__dirname, 'stylus', 'includes') ]
-    }))
-    .pipe(minifyCss({keepBreaks: false}))
-    .pipe(gulp.dest('front/css'))
-    .pipe(livereload());
-});
+gulp.task('build:stylus', modularGulp.createTask('stylus', styleFiles, 'front/css'));
+gulp.task('lint', modularGulp.createTask('lint', jsFiles));
+gulp.task('minify-concat:js', ['lint'], modularGulp.createTask('minify-concat', jsFiles, 'all.min.js', 'front/js/min'));
+gulp.task('bower-concat', ['copy-bower-map'], modularGulp.createTask('concat', jsComponents, 'components.min.js', 'front/js/min'));
+gulp.task('copy-bower-map', modularGulp.createTask('copy', jsComponentsMaps, 'front/js/min'));
+gulp.task('nodemon', modularGulp.createTask('nodemon', 'back/bin/www'));
+gulp.task('livereload', modularGulp.createTask('watch', 'back/modules/**/views/*.jade'));
+gulp.task('mocha', modularGulp.createTask('mocha', 'back/test/**/*.js'));
+gulp.task('imageopt', modularGulp.createTask('imageopt', 'front/image/original/*.*', 'front/image'));
 
-gulp.task("bower", function () {
-  gulp.src(jsComponents)
-    .pipe(sourcemaps.init())
-    .pipe(concat('components.js'))
-    .pipe(gulp.dest("front/js/min"));
-});
-
-gulp.task("bower-map", function () {
-  gulp.src(jsComponentsMaps)
-    .pipe(rename({dirname: ''}))
-    .pipe(gulp.dest("front/js/min"));
-});
-
-gulp.task('minify', ['lint'], function () {
-  gulp.src(jsFiles)
-    .pipe(sourcemaps.init())
-    .pipe(uglify().on('error', gutil.log)) // notice the event here
-    .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('front/js/min'))
-    .pipe(livereload());
-});
-
-gulp.task('images-opt', function () {
-  gulp.src('front/image/original/*.*')
-    .pipe(imagemin({
-      optimizationLevel: 5,
-      progressive: true
-    }))
-    .pipe(gulp.dest('front/image'));
-});
-
-gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch(jsFiles, ['minify']);
-  gulp.watch(styleFiles, ['stylus']);
-  gulp.watch(styleFilesModules, ['stylus']);
-  gulp.watch('back/modules/**/views/*.jade').on('change', function (file) {
-    livereload.changed(file.path);
-  });
-});
-
-gulp.task('demon', function () {
-  nodemon({
-    script: 'back/bin/www',
-    ext: 'js',
-    env: { 'NODE_ENV': 'development' }
-  });
-});
-
-gulp.task('default', ['demon', 'minify', 'bower', 'bower-map', 'stylus', 'watch']);
-gulp.task('imageopt', ['images-opt']);
+gulp.task('default', ['build:stylus', 'minify-concat:js', 'bower-concat', 'copy-bower-map', 'nodemon', 'livereload']);
