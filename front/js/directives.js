c24ae260a6bac749dcfa2771490b9242a4281fd2
Nicolas Ibanheiz <nicolas.ibanheiz@gmail.com>
2015-06-12 16:31:01 -0300

    Implementando atomic design

--- front/js/directives.js
+++ front/js/directives.js
@@ -1,9 +1,32 @@
 (function (angular) {
   'use strict';
 
+  function modalDialog() {
+    return {
+      restrict: 'E',
+      scope: {
+        show: '='
+      },
+      replace: true,
+      transclude: true,
+      link: function (scope, element, attrs) {
+        // Gambis para não mostrar modal ao carregar a tela
+        setTimeout(function () {
+          element.removeAttr('style');
+        }, 1000);
+
+        scope.hideModal = function () {
+          scope.show = false;
+        };
+      },
+      templateUrl: 'partials/template-modal'
+    };
+  }
+
   angular.module('app.directives',
     [
       'app.modules.Client.directives'
-    ]);
+    ])
+    .directive('modalDialog', modalDialog);
 
 }(angular));
--- front/js/modules/client/ClientDirectives.js
+++ front/js/modules/client/ClientDirectives.js
@@ -1,41 +1,6 @@
 (function (angular) {
   'use strict';
 
-  function modalDialog() {
-    return {
-      restrict: 'E',
-      scope: {
-        show: '='
-      },
-      replace: true,
-      transclude: true,
-      link: function (scope, element, attrs) {
-        scope.dialogStyle = {};
-        if (attrs.width) {
-          scope.dialogStyle.width = attrs.width;
-        }
-        if (attrs.minHeight) {
-          scope.dialogStyle.minHeight = attrs.minHeight;
-        }
-        if (attrs.padding) {
-          scope.dialogStyle.paddingTop =  attrs.padding;
-          scope.dialogStyle.paddingBottom =  attrs.padding;
-        }
-
-        // Gambis para não mostrar modal ao carregar a tela
-        setTimeout(function () {
-          element.removeAttr('style');
-        }, 1000);
-
-        scope.hideModal = function () {
-          scope.show = false;
-        };
-      },
-      templateUrl: 'partials/template-modal'
-    };
-  }
-
-  angular.module('app.modules.Client.directives', [])
-    .directive('modalDialog', modalDialog);
+  angular.module('app.modules.Client.directives', []);
 
 }(angular));
