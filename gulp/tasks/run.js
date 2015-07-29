25ce8f0f365058a18cfcb0fdb024afe15f15d73a
Nicolas Ibanheiz <nicolas.ibanheiz@gmail.com>
2015-07-29 16:02:27 -0300

    Criando task gulp para verificar node env

---
+++ gulp/tasks/run.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+
+gulp.task('run', ['clean'], function (cb) {
+
+  switch (process.env.NODE_ENV) {
+  case 'development':
+
+    global.isProd = false;
+    runSequence(['styles', 'fonts', 'images', 'browserify'], 'watch', cb);
+
+    break;
+  case 'production':
+
+    global.isProd = true;
+    runSequence(['styles', 'fonts', 'images', 'browserify'], 'gzip', cb);
+
+    break;
+
+  case 'test':
+    return runSequence('unit', 'protractor');
+
+  default:
+    console.log('**************************************');
+    console.log('********* NODE_ENV undefined *********');
+    console.log('**************************************');
+  }
+
+});
